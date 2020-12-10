41a8eb569fc7b112fabb9c2a09b659b9b0f6ddea
Tal Harouvi <halouvi@gmail.com>
2020-12-10 14:02:57 +0200

    shaboor

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import app from './app.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import VueYoutube from 'vue-youtube'
+ 
+Vue.use(VueYoutube)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(app)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="home">
+    <videoPlayer :videoId="videoId" />
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import videoPlayer from '@/components/videoPlayer.cmp'
 
 export default {
-  name: 'Home',
+  computed: {
+    videoId() {
+      return this.$store.getters.videoId
+    },
+  },
   components: {
-    HelloWorld
+    videoPlayer,
+  },
+  created() {
+    console.log(this.videoId);
   }
 }
 </script>
